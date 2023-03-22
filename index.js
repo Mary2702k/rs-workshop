9c107ae33ac31ffccaac07c4931a2f2da5aa7c82
github-mary2702k <yakimovamary@gmail.com>
2023-03-22 12:54:17 +0300

    feat: training for tests

---
+++ index.js
@@ -0,0 +1,123 @@
+// "use strict";
+
+// const details = {
+//     name: 'Hello!',
+// }
+
+// function getMessage() {
+//     return this.message;
+
+// "use strict";
+
+// const details = {
+//     message: 'Hello!',
+// }
+
+// function getMessage() {
+//     return this.message;
+// }
+
+// console.log(getMessage.call(details));
+
+// console.log(getMessage.call(details));
+
+// let name = 'John';
+
+// function printName() {
+//     let name = 'Peter';
+//     console.log(name);
+// }
+
+// printName();
+
+// var name = 'John';
+
+// var user = {
+//     name: "peter",
+//     printMessage() {
+//         console.log(`Hello, ${this.name}!`);
+//     }
+// };
+
+// var printMessage = user.printMessage;
+// printMessage();
+
+// function printName() {
+//     let name = 'Peter';
+//     console.log(name);
+// }
+
+// printName();
+
+// const details = {
+//     name: 'John!',
+// }
+// function getMessage(message) {
+//     return `${message} ${this.name}`;
+// }
+
+// console.log(getMessage.apply(details, ['Hello']));
+
+// for(let i = 0; i < 3; i++) {
+//     setTimeout(function() {
+//         console.log(i)
+//     }, 1000)
+// }
+// let a = 1;
+// let b = 2;
+// let f = function(a,b) { return a + b }
+// console.log(f);
+
+// "use strict";
+
+// function getThis() {
+//     return this;
+// }
+
+// console.log(getThis());
+
+// function foo(a, b) {
+//     return a * b;
+// }
+
+// const bar = foo.bind(null, 2);
+// bar(2);
+
+// printMessage();
+
+// function printMessage() {
+//     console.log('Hello!');
+// }
+
+// var a = 1, b = function a(x) { x && a(--x);};
+// console.log(a);
+
+// function foo() {
+//     return { bar: 1};
+// }
+
+// console.log(typeof foo().bar);
+
+// for(var i = 0; i < 10; i++) {
+//     //...
+// }
+// console.log(i);
+
+// function foo() { console.log(this);}
+// foo.call(null);
+
+// printMessage();
+
+// function printMessage() {
+//     console.log('Hello!');
+// }
+
+// let f = function g() { return 23;};
+// console.log(typeof
+
+
+
+
+
+
+ 
